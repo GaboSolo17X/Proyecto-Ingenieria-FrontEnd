e65a39649b2d63e20b24a17f277872bfd604af95
ArtOfNothing <aguilarericka60@gmail.com>
2023-11-08 21:47:06 -0600

    tabla de notas

---
+++ src/components/Docente/components/selectAsignatura.vue
@@ -0,0 +1,7 @@
+  <template >
+        <v-select
+  label="Seleccionar"
+  :items="['Ingenieria del software 0900', 'Seguridad Informatica 1100', 'Bases de datos 1000']"
+    variant="outlined"
+></v-select>
+  </template>
--- src/components/Docente/views/subirNotas.vue
+++ src/components/Docente/views/subirNotas.vue
@@ -1,19 +1,19 @@
 <template>
-  <div >
-    <v-row>
-      <v-col cols="4">
-        <Lateral/>
-      </v-col>
-      <v-col cols="8">
-        <div class="contenido">
-          <Encabezado  title="Subida de notas"/>
+    <div >
+      <v-row>
+        <v-col cols="4">
+          <Lateral/>
+        </v-col>
+        <v-col cols="8">
+          <div class="contenido">
+            <Encabezado  title="Subida de Notas"/>
+          </div>
+        <div class="asignatura">
+          <SelectAsignatura />
         </div>
-        <h1 class="componentesDocentes">Aqui va una tabla</h1>
-        <v-img
-        width="300"
-        :aspect-ratio="1"
-        src="../assets/tabla.jpeg"
-      ></v-img>
+        <div class="componentesDocentes">
+        <TablaNotas/>
+        </div>
       </v-col>
     </v-row>
   </div>
@@ -23,16 +23,28 @@
 <script>
 import Lateral from '../components/lateral.vue'
 import Encabezado from  '../components/encabezado.vue'
+import TablaNotas from '../components/tablaNotas.vue'
+import SelectAsignatura from '../components/selectAsignatura.vue'
 
 export default {
-components: {Lateral,Encabezado},
+components: {Lateral,Encabezado, TablaNotas, SelectAsignatura},
 }
+
+
 </script>
 
 <style scoped>
   @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;500&display=swap');
+
+.asignatura{
+    width: 270px;
+    margin-top: 100px;
+    position: relative;
+    right: 100px !important;
+}
+
 .componentesDocentes{
-margin-top: 100px;
+margin-top: 10px;
 left: 230px !important;
 width: calc((100% - 230px) - 0px);
 }
